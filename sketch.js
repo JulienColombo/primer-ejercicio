40e4480ca4b292705e4f3e7ceba53dc64d9522af
Julien <84051054+JulienColombo@users.noreply.github.com>
2023-05-27 12:27:47 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+function setup() {
+  createCanvas(820, 820);
+  //fondo
+  background(200) ;
+  
+  
+  // ancho de linea
+  strokeWeight(50);
+  
+  // circunferencia y su color
+    stroke(100, 200, 130);
+    ellipse(410, 410, 750, 750);
+  
+  stroke(75, 150, 75);
+  ellipse(410, 410, 700, 750);
+  
+  stroke(50, 100, 30);
+   ellipse(410, 410, 650, 750);
+  
+  stroke(0, 50, 75);
+   ellipse(410, 410, 600, 750);
+  
+  stroke(50, 100, 150);
+   ellipse(410, 410, 550, 750);
+  
+  stroke(100, 0, 50);
+   ellipse(410, 410, 500, 750);
+  
+   stroke(125, 25, 75);
+   ellipse(410, 410, 450, 750);
+  
+  stroke(150, 50, 75);
+   ellipse(410, 410, 400, 750);
+  
+  stroke(200, 50, 75);
+   ellipse(410, 410, 350, 750);
+  
+  stroke(200, 75, 100);
+  ellipse(410, 410, 300, 750);
+  
+  stroke(220, 150, 50);
+  ellipse(410, 410, 250, 750);
+  
+  stroke(225, 175, 25);
+  ellipse(410, 410, 200, 750);
+  
+  stroke(250, 200, 5);
+  ellipse(410, 410, 150, 750);
+  
+   stroke(150, 200, 75);
+  ellipse(410, 410, 100, 750);
+  
+  stroke(125, 200, 100);
+  ellipse(410, 410, 50, 750);
+  
+  stroke(100, 200, 130);
+  ellipse(410, 410, 1, 750);
+  
+  
+  	
+}
+
+
+
+
